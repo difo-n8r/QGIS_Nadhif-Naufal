f45fb1e80ff62d298791c61ba8c0ff98ad47c03a
Nadhif Naufal <33833954+difo-n8r@users.noreply.github.com>
2020-11-15 12:08:50 +0700

    Add files via upload

---
+++ layers/gunung_riau_4.js
@@ -0,0 +1,13 @@
+var json_gunung_riau_4 = {
+"type": "FeatureCollection",
+"name": "gunung_riau_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NM_GUNUNG": "BT. GUDANG", "TINGGI": "754" }, "geometry": { "type": "Point", "coordinates": [ 100.902327647044771, -0.09651601465512 ] } },
+{ "type": "Feature", "properties": { "NM_GUNUNG": "BT. MEREMBUN", "TINGGI": "927" }, "geometry": { "type": "Point", "coordinates": [ 101.163627646996844, -0.315562413972886 ] } },
+{ "type": "Feature", "properties": { "NM_GUNUNG": null, "TINGGI": "0" }, "geometry": { "type": "Point", "coordinates": [ 100.807727648257767, -0.250096213931845 ] } },
+{ "type": "Feature", "properties": { "NM_GUNUNG": "BT. JANJANG KAMBING", "TINGGI": "763" }, "geometry": { "type": "Point", "coordinates": [ 102.438438103307675, -1.018259698680067 ] } },
+{ "type": "Feature", "properties": { "NM_GUNUNG": null, "TINGGI": "0" }, "geometry": { "type": "Point", "coordinates": [ 100.967927647696101, -0.252133514971064 ] } },
+{ "type": "Feature", "properties": { "NM_GUNUNG": "BT. HULUPAWAN", "TINGGI": "1036" }, "geometry": { "type": "Point", "coordinates": [ 100.237299999461527, 0.71440730041934 ] } }
+]
+}
